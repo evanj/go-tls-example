3a351af1d42b59a9ef3edc5e3688bc131d80c111
Evan Jones <evan.jones@bluecore.com>
2019-11-20 14:26:17 -0500

    Initial TLS: client fails to connect; server starts listening

--- echoserver/echoserver.go
+++ echoserver/echoserver.go
@@ -6,7 +6,6 @@ import (
 	"crypto/elliptic"
 	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
 	"flag"
 	"fmt"
 	"net"
@@ -37,98 +36,6 @@ import (
 // openssl x509 -req -in serverkey.csr -CA cacert.pem -CAkey cakey.pem -CAcreateserial -out servercert.pem -days 1095
 // check it with: openssl x509 -text -in cert.pem -noout
 
-const key = `-----BEGIN EC PARAMETERS-----
-BggqhkjOPQMBBw==
------END EC PARAMETERS-----
------BEGIN EC PRIVATE KEY-----
-MHcCAQEEIOY36v3ybwSq3/bd8XdFVf8aYaNWAGYZZXKSBU+qmTqXoAoGCCqGSM49
-AwEHoUQDQgAEgKXyK9/QMEnpejKnf3qWGkkR3rv67UxsnfWiW6vYp7Mh9xs968eN
-hB1/5C7SfbLQGyY84vAm1jb10YZWZD+Hsg==
------END EC PRIVATE KEY-----`
-
-const certificateWithCN = `-----BEGIN CERTIFICATE-----
-MIIBnTCCAUOgAwIBAgIJAImDoLNBzG5pMAoGCCqGSM49BAMCMCsxFTATBgNVBAoM
-DEJsdWVjb3JlIEluYzESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTE2MTIxNjIxMDUw
-NFoXDTE5MTIxNjIxMDUwNFowKzEVMBMGA1UECgwMQmx1ZWNvcmUgSW5jMRIwEAYD
-VQQDDAlsb2NhbGhvc3QwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASApfIr39Aw
-Sel6Mqd/epYaSRHeu/rtTGyd9aJbq9insyH3Gz3rx42EHX/kLtJ9stAbJjzi8CbW
-NvXRhlZkP4eyo1AwTjAdBgNVHQ4EFgQU8HojMJXOYvIpbPyCeuWUvNSA/SwwHwYD
-VR0jBBgwFoAU8HojMJXOYvIpbPyCeuWUvNSA/SwwDAYDVR0TBAUwAwEB/zAKBggq
-hkjOPQQDAgNIADBFAiB3wawLITpnnq/PPj2boVcrn9V/23P3AMHMKb7FH/E/ogIh
-APCY7jmCeJ1sbnrvAth0KeYl9niP47DMMilqZt3Qt6HO
------END CERTIFICATE-----`
-
-// RSA for older clients (e.g. OpenSSL with Mac OS X 10.11)
-// openssl genrsa -out serverkey_rsa.pem 2048
-// openssl req -new -x509 -sha256 -key serverkey_rsa.pem -out serverkey_rsa_cert.pem -days 1095 -subj "/O=Bluecore Inc/CN=localhost"
-const rsaKey = `-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAwe9/DhNezYq9fUVH4k9H8K+bjoaRoAKI3pd9Jle2G1bSF7FV
-w9UOUPTGyN0NNxkTo7n0aKG4muxiTMf+J4Vev7M+39qkWO05J/SaqRsf/jmpZrbx
-okf5ZYy70RB3K40e5wnuIiRpYulORD4CKbR2D7cqnh+kS2jklz6pzG+cg/1/36Rf
-eZzTJBClRaA3kZfazfSIILcBwcxkkJneyz7yY3Ims0uoaiIUBggfLaplkw2tGgUC
-XqYGnpX8ZwP5tLlSs94+T6xb6/odFJSxpR6JL8U/OP6wM+ZmQq3wT+ccSdmaj19+
-zsfW6TsTc9mnMb91hWZxYX6tz3Vo/bdQ5R/Y4QIDAQABAoIBAG5oKE8dG/WsLKBq
-lrfyJqSRdN+5IKKVjtIaMDdp69S4ora2UHtCifnwwFKN9f4OGq41KT2ABBa+yF+p
-47kgJobARuUuFi5CLy3eBUaMmLQko5pEQGAA1azIaAxncVCr8JHbh2SMkiqrY9FZ
-8+VGRYhA4NRQczozJyArnoMyb0p8Q1JvsJOyMWyM5jyBr8d4sYDoilI1kMBMnVLw
-QSxJxRT4LEamNK74HZiC87kQ6FKeN0pknKuma9F4zNi1k1LOHLkDoV0tdf9xs+dp
-NGmsCaIyZKB47G/EVvlpj4Sd0XiQCLmj1eijCJv/koV1oNCkE8hztwJFpzHIPVXp
-g9NnWKECgYEA49ed9VukVn0vNKUoSfO5OW7Gw1AmQ8JUflqzH8Jq/WmrPzt9eP/J
-Wp6k+uGcWZ1pKE+tBN0NMWxl/ZOlnOFlnALRzmnFWCfgOlMqubSyTBLTbhV9xK0X
-FpXkG9M21kg6qL6yv7KmkPmwqJmJ7RuWjw+wISBSq2fVZfPrqwQZcw0CgYEA2ecm
-4DhhwtNTpmIjxxMu44FHoMJ1he1fS/f5FEb1Z2RQCrroKsvlnOyzfM82srIRTOui
-09P+zBy8AfGxd9DVVE3+mhObi2yNIoSdMnmQ5bJi6kXla4daeD3O1R36WU/RMFo6
-mlRRkbFK5ATBVismTb+H6A6Vu+aNOzf3k58NGCUCgYEAsfiish07aTOvw/eeWOXA
-MVSOfdIFkbgiN/CxKoW6/OellGbrw7BJoDd7t1yzvGxwz8Qs6jehLpH1uPWzz69g
-p1Ssfgew1wOO6wA7x4OWIkYyUTMIYrCx8Dp1TCbYFtmkuFr9VTA/W88uZwRH8KY0
-HNusQLP231zkHBdEZvl0x6ECgYEAk4FSY75HD2eW3K7aoUxvFTrSjhSVEdAaDocS
-aZOPVContNvJhg74pD3nUrnCwTyhUXzBgLulY+6fpoFCLWWEw8j4bLyRMtSMxa0X
-7K82UwdqxkQChcPejj7o4IOmkQbjCORLf2VMwl9N3wUJC5eyGjdpfMFCwyvStx+v
-zJKCfrECgYBqli0WRMLB8/pblrZLYH3fFeYbh7w0jiu5kpeqiPHfRtLrXGLz3q4Q
-W+udwvNEezccl6vPWnjgXhjMOz9BkbbQXu25IdZZCoitAuoYaUL6BPeg2JSBqIDH
-phXsf/9zVEjfvVW1VHVXHY8DI0ytdBsk8YPmUpFmRezPWzeTCtYchw==
------END RSA PRIVATE KEY-----`
-
-const rsaCert = `-----BEGIN CERTIFICATE-----
-MIIDZzCCAk+gAwIBAgIJANG/mbEbHoJrMA0GCSqGSIb3DQEBCwUAMCsxFTATBgNV
-BAoTDEJsdWVjb3JlIEluYzESMBAGA1UEAxMJbG9jYWxob3N0MB4XDTE2MTIxNzE4
-MzAxMFoXDTE5MTIxNzE4MzAxMFowKzEVMBMGA1UEChMMQmx1ZWNvcmUgSW5jMRIw
-EAYDVQQDEwlsb2NhbGhvc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
-AQDB738OE17Nir19RUfiT0fwr5uOhpGgAojel30mV7YbVtIXsVXD1Q5Q9MbI3Q03
-GROjufRoobia7GJMx/4nhV6/sz7f2qRY7Tkn9JqpGx/+OalmtvGiR/lljLvREHcr
-jR7nCe4iJGli6U5EPgIptHYPtyqeH6RLaOSXPqnMb5yD/X/fpF95nNMkEKVFoDeR
-l9rN9IggtwHBzGSQmd7LPvJjciazS6hqIhQGCB8tqmWTDa0aBQJepgaelfxnA/m0
-uVKz3j5PrFvr+h0UlLGlHokvxT84/rAz5mZCrfBP5xxJ2ZqPX37Ox9bpOxNz2acx
-v3WFZnFhfq3PdWj9t1DlH9jhAgMBAAGjgY0wgYowHQYDVR0OBBYEFDmlNJnIGO2u
-kOBSKIabT/Aqi+fTMFsGA1UdIwRUMFKAFDmlNJnIGO2ukOBSKIabT/Aqi+fToS+k
-LTArMRUwEwYDVQQKEwxCbHVlY29yZSBJbmMxEjAQBgNVBAMTCWxvY2FsaG9zdIIJ
-ANG/mbEbHoJrMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAGlfnhSn
-UXT2MNj3rT5wVi4fkBf9HmQCjwU0wxgXz9n5ussNr1Anj5L52fh9sxXz5hnpeyaz
-yXnrT7F3W0lP8CxombX8ORbENsgQGab4OGi3YsSn2ZZNsVVgM4QnNwwkqqL42QSw
-UzhnpUalgtckomALDyRgjLVtevN+E+Z4eu2v71lKrPUKOo1W7dTRmuxzV7z5Qabm
-TrxgH3gZHeBKs/MY2FB95V4A//hRYWeH0H1xUUKOEiH6tkN8UumEmr+cNBs7QFbD
-ZypgfhagyCdw+cyTlQRlvVnjQZo8ClL8Q25/xGvvJOFBlyY6U5EfJCzeYgXkNUcp
-3Cxn3O56lPYWonk=
------END CERTIFICATE-----`
-
-func listenTLS(addr string, authType tls.ClientAuthType, certPool *x509.CertPool) (net.Listener, error) {
-	cert, err := tls.X509KeyPair([]byte(rsaCert), []byte(rsaKey))
-	// cert, err := tls.X509KeyPair([]byte(certificateWithCN), []byte(key))
-	if err != nil {
-		return nil, err
-	}
-	config := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		ClientAuth:   authType,
-		ClientCAs:    certPool,
-	}
-	return tls.Listen("tcp", addr, config)
-}
-
-func listenNoTLS(addr string) (net.Listener, error) {
-	return net.Listen("tcp", addr)
-}
-
 // Returns a unique binary representation for pub. This can be used to identify specific clients.
 func marshalPublicKey(pub interface{}) ([]byte, error) {
 	var publicKeyBytes []byte
@@ -147,6 +54,8 @@ func marshalPublicKey(pub interface{}) ([]byte, error) {
 }
 
 func handleConn(conn net.Conn) {
+	defer conn.Close()
+
 	clientAddrString := conn.RemoteAddr().String()
 	fmt.Printf("echoserver connection from %s\n", clientAddrString)
 	scanner := bufio.NewScanner(conn)
@@ -160,15 +69,30 @@ func handleConn(conn net.Conn) {
 		}
 	}
 	if scanner.Err() != nil {
-		panic(scanner.Err())
+		fmt.Printf("echoserver ERROR from %s closing connection: %s\n", clientAddrString, scanner.Err().Error())
+		return
 	}
 
+	err := conn.Close()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("echoserver connection from %s closed\n", clientAddrString)
+}
+
+func listenTLS(addr string, serverCertPath string, serverKeyPath string) (net.Listener, error) {
+	cert, err := tls.LoadX509KeyPair(serverCertPath, serverKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	cfg := &tls.Config{Certificates: []tls.Certificate{cert}}
+	return tls.Listen("tcp", addr, cfg)
 }
 
 func main() {
 	addr := flag.String("addr", ":7000", "listen address")
-	useTLS := flag.Bool("useTLS", true, "require TLS connections")
+	certPath := flag.String("cert", "", "path to the server cert")
+	keyPath := flag.String("key", "", "path to the server key")
 	// msglen := flag.Int("msglen", 0, "If > 0, read a fixed size message (needed for TLS: no CloseWrite)")
 	// requestClientCert := flag.Bool("requestClientCert", false, "Request client certificate")
 	// requireClientCert := flag.Bool("requireClientCert", false, "Require client certificate")
@@ -177,11 +101,16 @@ func main() {
 	// 	"Correct clients (e.g. Go) only send matching certificates, so this may cause no certificate errors.")
 	flag.Parse()
 
-	if *useTLS {
-		panic("todo")
+	var listener net.Listener
+	var err error
+	if *certPath != "" && *keyPath != "" {
+		fmt.Printf("echoserver listening for TLS with addr=%s cert=%s key=%s\n",
+			*addr, *certPath, *keyPath)
+		listener, err = listenTLS(*addr, *certPath, *keyPath)
+	} else {
+		fmt.Printf("echoserver listening without TLS with addr=%s\n", *addr)
+		listener, err = net.Listen("tcp", *addr)
 	}
-
-	listener, err := net.Listen("tcp", *addr)
 	if err != nil {
 		panic(err)
 	}
--- endtoend/endtoend.go
+++ endtoend/endtoend.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -17,26 +18,31 @@ const startupConnectSleep = 100 * time.Millisecond
 const startupTotalDuration = 2 * time.Second
 
 const echoclientInput = "hello world\n"
-const expectedEchoclientOutput = "echoclient received from server: " + echoclientInput
+const expectedSuccessOutput = "echoclient received from server: " + echoclientInput
 
 func isConnRefused(err error) bool {
 	return errors.Is(err, syscall.ECONNREFUSED)
 }
 
-func main() {
-	echoclientPath := flag.String("echoclientPath", "", "path to the echoclient binary")
-	echoserverPath := flag.String("echoserverPath", "", "path to the echoserver binary")
-	localhostPort := flag.Int("localhostPort", 7123, "port to listen on locally")
-	flag.Parse()
+type testConfiguration struct {
+	name           string
+	clientArgs     []string
+	serverArgs     []string
+	expectedOutput string
+	expectFailure  bool
+}
 
-	listenAddr := fmt.Sprintf("%s:%d", localhostAddr, *localhostPort)
+func runTest(
+	echoclientPath string, echoserverPath string, listenAddr string, config *testConfiguration,
+) error {
 	fmt.Printf("starting server listening on %s ...\n", listenAddr)
-	serverProc := exec.Command(*echoserverPath, "--useTLS=false", "--addr="+listenAddr)
+	serverArgs := append([]string{"--addr=" + listenAddr}, config.serverArgs...)
+	serverProc := exec.Command(echoserverPath, serverArgs...)
 	serverProc.Stdout = os.Stdout
 	serverProc.Stderr = os.Stderr
 	err := serverProc.Start()
 	if err != nil {
-		panic(err)
+		return err
 	}
 	// ensure we stop the server
 	defer serverProc.Process.Kill()
@@ -52,7 +58,7 @@ func main() {
 				// try again if the connection was refused: server has not started yet
 				continue
 			}
-			panic(err)
+			return err
 		}
 
 		// success the server exists!
@@ -61,16 +67,60 @@ func main() {
 	}
 
 	fmt.Printf("starting client ...\n")
-	clientProc := exec.Command(*echoclientPath, "--useTLS=false", "--addr="+listenAddr)
+	clientArgs := append([]string{"--addr=" + listenAddr}, config.clientArgs...)
+	clientProc := exec.Command(echoclientPath, clientArgs...)
 	clientProc.Stdin = bytes.NewBufferString(echoclientInput)
 	output, err := clientProc.CombinedOutput()
 	fmt.Println("=== start echoclient output ===")
 	os.Stdout.Write(output)
 	fmt.Println("=== end echoclient output ===")
 	if err != nil {
-		panic(err)
+		exitErr := &exec.ExitError{}
+		if errors.As(err, &exitErr) && config.expectFailure {
+			fmt.Printf("expected exit failure: %s\n", exitErr.Error())
+		} else {
+			return err
+		}
 	}
-	if !bytes.Contains(output, []byte(expectedEchoclientOutput)) {
-		panic("echoclient did not contain expected output")
+	if !bytes.Contains(output, []byte(config.expectedOutput)) {
+		return errors.New("echoclient did not contain expected output")
+	}
+	return nil
+}
+
+func main() {
+	echoclientPath := flag.String("echoclientPath", "", "path to the echoclient binary")
+	echoserverPath := flag.String("echoserverPath", "", "path to the echoserver binary")
+	serverCertPath := flag.String("serverCertPath", "", "path to the server certificate")
+	serverKeyPath := flag.String("serverKeyPath", "", "path to the server key")
+	localhostPort := flag.Int("localhostPort", 7123, "port to listen on locally")
+	flag.Parse()
+
+	listenAddr := fmt.Sprintf("%s:%d", localhostAddr, *localhostPort)
+	fmt.Printf("using listenAddr=%s\n", listenAddr)
+
+	serverCertKeyArgs := []string{"--cert=" + *serverCertPath, "--key=" + *serverKeyPath}
+
+	configs := []*testConfiguration{
+		{"no TLS (plain TCP)", []string{"--useTLS=false"}, []string{}, expectedSuccessOutput, false},
+
+		// client fails due to not using TLS
+		{"server TLS; client no TLS", []string{"--useTLS=false"}, serverCertKeyArgs,
+			"panic: unexpected end of data from server", true},
+
+		// client fails due to using global trusted root CAs
+		{"server TLS; client global trusted TLS", []string{}, serverCertKeyArgs,
+			"panic: x509: certificate signed by unknown authority", true},
+	}
+
+	for i, config := range configs {
+		fmt.Printf("starting test %d %s; client=%s server=%s ...\n",
+			i, config.name, strings.Join(config.clientArgs, " "), strings.Join(config.serverArgs, " "))
+		err := runTest(*echoclientPath, *echoserverPath, listenAddr, config)
+		if err != nil {
+			fmt.Printf("TEST FAILED: %s\n", err.Error())
+			os.Exit(42)
+		}
+		fmt.Printf("SUCCESS\n\n")
 	}
 }
